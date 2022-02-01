fb4bcc35528e7c00b9404fbbf24bf0e7fbb29362
Nailton <51789882+nailtonvital@users.noreply.github.com>
2022-02-01 14:27:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body"),
+    sidebar = document.querySelector(".sidebar"),
+    toggle = document.querySelector(".toggle"),     
+    modeSwitch = document.querySelector(".toggle-switch"),
+    modeText = document.querySelector(".mode-text");
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+    });
